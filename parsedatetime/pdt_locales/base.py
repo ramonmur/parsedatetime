1fe8218aeb547562bc3a6ab1dcf8447f72116199
Ramon Mur <rmur@bovisync.com>
2020-04-16 15:46:07 -0500

    Add all numbers in spanish locale and twelve in base locale

--- parsedatetime/pdt_locales/base.py
+++ parsedatetime/pdt_locales/base.py
@@ -71,6 +71,7 @@ numbers = {
     'nine': 9,
     'ten': 10,
     'eleven': 11,
+    'twelve': 12,
     'thirteen': 13,
     'fourteen': 14,
     'fifteen': 15,
--- parsedatetime/pdt_locales/es.py
+++ parsedatetime/pdt_locales/es.py
@@ -51,3 +51,29 @@ dayOffsets = {
     'hoy': 0,
     'ayer': -1,
 }
+
+
+numbers = {
+    'cero': 0,
+    'uno': 1,
+    'un': 1,
+    'dos': 2,
+    'tres': 3,
+    'cuatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'siete': 7,
+    'ocho': 8,
+    'nueve': 9,
+    'diez': 10,
+    'once': 11,
+    'doce': 12,
+    'trece': 13,
+    'catorce': 14,
+    'quince': 15,
+    'dieciseis': 16,
+    'diecisiete': 17,
+    'dieciocho': 18,
+    'diecinueve': 19,
+    'veinte': 20
+}
